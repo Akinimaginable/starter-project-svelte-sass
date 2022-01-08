99e4622f1dfc4554fb3e5ce6657981bce4e038bf
Akinimaginable 秋 <38192770+Akinimaginable@users.noreply.github.com>
2022-01-08 19:42:18 +0100

    Add files via upload

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+    <title>Index</title>
+</svelte:head>
+
+<h1>Svelte starter project with SCSS preinstalled</h1>
+
+<style lang="scss">
+    h1 {
+        color: $h1-color;
+        text-shadow: .1rem .1rem .3rem $h1-background;
+        font-size: 2rem;
+    }
+</style>
